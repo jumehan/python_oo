409c590aec1fc1514aa776a36d2ffd043e675bc9
Julie Han <juliemenghan@gmail.com>
2022-07-26 19:03:46 -0400

    wordfinder done

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,23 @@
+from random import choice
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.word_list = []
+        self.word = ''
+        self.read_list()
+
+    def __repr__(self):
+        ...
+
+    def random(self):
+        self.word = choice(self.word_list)
+        self.word = self.word.replace("\n", "")
+        return self.word
+
+    def read_list(self):
+        f = open(self.file_path)
+        self.word_list = f.readlines()
+        print(f"{len(self.word_list)} words read")
